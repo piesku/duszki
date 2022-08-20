0244d2de4687d31a536e0dcdc2c57a4af34088ce
Stanisław Małolepszy <sta@malolepszy.org>
2022-08-20 22:05:24 +0200

    Zoom to pointer

--- src/systems/sys_control_camera.ts
+++ src/systems/sys_control_camera.ts
@@ -1,31 +1,59 @@
+import {pointer_viewport} from "../../lib/input.js";
+import {Vec2} from "../../lib/math.js";
 import {clamp} from "../../lib/number.js";
+import {scale, subtract} from "../../lib/vec2.js";
+import {viewport_to_world} from "../components/com_camera2d.js";
 import {Game} from "../game.js";
 import {Has} from "../world.js";
 
 const QUERY = Has.Camera2D | Has.LocalTransform2D;
+const pointer_position: Vec2 = [0, 0];
 let wheel_y_clamped = 0;
 
 export function sys_control_camera(game: Game, delta: number) {
+    let camera_entity = game.Cameras[0];
+    if (camera_entity === undefined) {
+        // This system requires the main camera to exist.
+        return;
+    }
+
+    let camera = game.World.Camera2D[camera_entity];
+    let camera_local = game.World.LocalTransform2D[camera_entity];
+
     if (game.InputDelta["WheelY"]) {
-        wheel_y_clamped = clamp(-1000, 500, wheel_y_clamped + game.InputDelta["WheelY"]);
-        let zoom = 4 ** (wheel_y_clamped / -500);
-        if (0.85 < zoom && zoom < 1.15) {
-            zoom = 1;
+        let cur_zoom = 4 ** (wheel_y_clamped / -500);
+        if (0.85 < cur_zoom && cur_zoom < 1.15) {
+            cur_zoom = 1;
         }
-        game.UnitSize = 16 * zoom;
+
+        wheel_y_clamped = clamp(-1000, 500, wheel_y_clamped + game.InputDelta["WheelY"]);
+        let new_zoom = 4 ** (wheel_y_clamped / -500);
+        if (0.85 < new_zoom && new_zoom < 1.15) {
+            new_zoom = 1;
+        }
+
+        game.UnitSize = 16 * new_zoom;
         game.ViewportResized = true;
+
+        if (pointer_viewport(game, pointer_position)) {
+            // Position under the mouse cursor at cur_zoom.
+            viewport_to_world(pointer_position, camera, pointer_position);
+
+            let offset: Vec2 = [0, 0];
+            subtract(offset, pointer_position, camera_local.Translation);
+            scale(offset, offset, 1 - cur_zoom / new_zoom);
+
+            camera_local.Translation[0] += offset[0];
+            camera_local.Translation[1] += offset[1];
+            game.World.Signature[camera_entity] |= Has.Dirty;
+        }
     }
 
     if (game.InputDistance["Mouse0"] > 5) {
         document.body.classList.add("grabbing");
-        for (let ent = 0; ent < game.World.Signature.length; ent++) {
-            if ((game.World.Signature[ent] & QUERY) === QUERY) {
-                let local = game.World.LocalTransform2D[ent];
-                local.Translation[0] -= game.InputDelta["MouseX"] / game.UnitSize;
-                local.Translation[1] += game.InputDelta["MouseY"] / game.UnitSize;
-                game.World.Signature[ent] |= Has.Dirty;
-            }
-        }
+        camera_local.Translation[0] -= game.InputDelta["MouseX"] / game.UnitSize;
+        camera_local.Translation[1] += game.InputDelta["MouseY"] / game.UnitSize;
+        game.World.Signature[camera_entity] |= Has.Dirty;
     }
 
     if (game.InputDelta["Mouse0"] === -1) {
