6e6b647b39f3f547cc0dbd4995bf383a409bf7e1
Stanisław Małolepszy <sta@malolepszy.org>
2022-08-24 21:30:46 +0200

    Reset the world grid for all the building's tiles when erasing it

--- src/systems/sys_build_erase.ts
+++ src/systems/sys_build_erase.ts
@@ -1,5 +1,5 @@
 import {pointer_clicked, pointer_down} from "../../lib/input.js";
-import {destroy_all} from "../components/com_children.js";
+import {destroy_all, query_down} from "../components/com_children.js";
 import {Game} from "../game.js";
 import {Has} from "../world.js";
 import {make_road} from "./sys_build_roads.js";
@@ -23,6 +23,18 @@ export function sys_build_erase(game: Game, delta: number) {
                     if (game.World.Signature[cell.tile_entity] & Has.SpatialNode2D) {
                         let spatial = game.World.SpatialNode2D[cell.tile_entity];
                         if (spatial.Parent !== undefined) {
+                            // Reset the world grid for all the building's tiles.
+                            for (let child_entity of query_down(
+                                game.World,
+                                spatial.Parent,
+                                Has.Render2D
+                            )) {
+                                let child_spatial = game.World.SpatialNode2D[child_entity];
+                                let child_x = Math.round(child_spatial.World[4]);
+                                let child_y = Math.round(child_spatial.World[5]);
+                                game.World.Grid[child_y][child_x].tile_entity = null;
+                                game.World.Grid[child_y][child_x].walkable = false;
+                            }
                             // Destroy the building's root entity.
                             destroy_all(game.World, spatial.Parent);
                         } else {
