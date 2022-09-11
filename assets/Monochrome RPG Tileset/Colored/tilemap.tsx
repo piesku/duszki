179a848410fc1f42f896ae1012a4aed47b799298
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-11 18:35:23 +0200

    Remove unused sprites, part 1

---
+++ assets/Monochrome RPG Tileset/Colored/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Duszki" tilewidth="16" tileheight="16" tilecount="136" columns="17">
+ <image source="tilemap_packed.png" width="272" height="128"/>
+</tileset>
