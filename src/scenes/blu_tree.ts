2d0e26022a86e7ebc7f060efbd8c49d0695e0194
Michał Budzyński <michal@virtualdesign.pl>
2022-09-11 21:53:11 +0200

    single tree tile

--- src/scenes/blu_tree.ts
+++ src/scenes/blu_tree.ts
@@ -1,15 +1,12 @@
-import {element} from "../../lib/random.js";
 import {control_player} from "../components/com_control_player.js";
 import {local_transform2d} from "../components/com_local_transform2d.js";
 import {render2d, shift} from "../components/com_render2d.js";
 import {Game} from "../game.js";
 
-export const TREE_TILES = ["015.png"];
-
 export function blueprint_tree(game: Game) {
-    return [local_transform2d(), render2d(element(TREE_TILES))];
+    return [local_transform2d(), render2d("015.png")];
 }
 
 export function blueprint_tree_phantom(game: Game) {
-    return [local_transform2d(), control_player("tree"), render2d(element(TREE_TILES)), shift(5)];
+    return [local_transform2d(), control_player("tree"), render2d("015.png"), shift(5)];
 }
