8c6c506b492fa5a160485d052a933ad2a4670829
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-13 00:17:05 +0200

    Prettify map files

--- maps/map_food.ts
+++ maps/map_food.ts
@@ -1,2 +1,5 @@
-// prettier-ignore
-export const map_food = {"Width":3,"Height":5,"Tiles":[92,109,363,130,79,132,81,114,115,96,97,98,null,80,null]};
+export const map_food = {
+    Width: 3,
+    Height: 5,
+    Tiles: [92, 109, 363, 130, 79, 132, 81, 114, 115, 96, 97, 98, null, 80, null],
+};
