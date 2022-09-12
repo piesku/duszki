0ad564bef2f2ebae6523db31f5cd62899a04b9ad
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-12 15:41:11 +0200

    Simplify grass tiles

--- src/scenes/blu_grass.ts
+++ src/scenes/blu_grass.ts
@@ -3,7 +3,7 @@ import {local_transform2d} from "../components/com_local_transform2d.js";
 import {render2d} from "../components/com_render2d.js";
 import {Game} from "../game.js";
 
-const grass_tiles = ["017.png", "034.png", "051.png", "068.png"];
+const grass_tiles = ["017.png", "017.png", "051.png", "051.png", "068.png"];
 
 export function blueprint_grass(game: Game) {
     return [local_transform2d(), render2d(element(grass_tiles))];
