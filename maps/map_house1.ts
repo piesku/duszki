becabd2334dc4a0121e2f7b1b958f41d347a8ddf
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-12 15:30:57 +0200

    Remove right-sided building tiles, flip left ones instead

--- maps/map_house1.ts
+++ maps/map_house1.ts
@@ -1,2 +1,2 @@
 // prettier-ignore
-export const map_house1 = {"Width":3,"Height":5,"Tiles":[93,111,109,131,80,133,82,115,116,97,98,99,0,81,0]};
+export const map_house1 = {"Width":3,"Height":5,"Tiles":[93,110,2147483756,131,80,133,82,115,116,97,98,99,0,81,0]};
--- maps/map_house3.ts
+++ maps/map_house3.ts
@@ -1,2 +1,2 @@
 // prettier-ignore
-export const map_house3 = {"Width":3,"Height":4,"Tiles":[59,43,61,25,26,27,25,26,27,8,9,10]};
+export const map_house3 = {"Width":3,"Height":4,"Tiles":[59,43,2147483707,25,26,2147483673,25,26,2147483673,8,9,2147483656]};
