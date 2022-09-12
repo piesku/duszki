d2e54adb8950e97c48d3a37e564f2d814f76a4e9
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-12 23:49:13 +0200

    Remove FPS measurements

--- lib/game.ts
+++ lib/game.ts
@@ -2,9 +2,6 @@ import {RenderTarget} from "./framebuffer.js";
 import {GL_CULL_FACE, GL_DEPTH_TEST, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA} from "./webgl.js";
 import {create_entity, Entity, WorldImpl} from "./world.js";
 
-const update_span = document.getElementById("update");
-const delta_span = document.getElementById("delta");
-const fps_span = document.getElementById("fps");
 const step = 1 / 60;
 
 /**
@@ -138,17 +135,6 @@ export abstract class GameImpl {
 
         for (let name in this.InputDelta) {
             this.InputDelta[name] = 0;
-        }
-
-        let update = performance.now() - this.Now;
-        if (update_span) {
-            update_span.textContent = update.toFixed(1);
-        }
-        if (delta_span) {
-            delta_span.textContent = (delta * 1000).toFixed(1);
-        }
-        if (fps_span) {
-            fps_span.textContent = (1 / delta).toFixed();
         }
     }
 }
