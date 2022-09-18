ed6cc8cc9573edd5d4eabf18f5e03b98fdbb22af
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-18 11:46:57 +0200

    Unquote map properties

--- maps/map_sleep.ts
+++ maps/map_sleep.ts
@@ -1,2 +1,2 @@
 // prettier-ignore
-export const map_sleep = {"Width":3,"Height":3,"Tiles":[21,12,277,7,8,263,1,2,257]};
+export const map_sleep = {Width:3,Height:3,Tiles:[21,12,277,7,8,263,1,2,257]};
--- maps/map_work.ts
+++ maps/map_work.ts
@@ -1,2 +1,2 @@
 // prettier-ignore
-export const map_work = {"Width":4,"Height":4,"Tiles":[35,43,42,291,11,41,36,297,1,11,12,267,null,1,2,257]};
+export const map_work = {Width:4,Height:4,Tiles:[35,43,42,291,11,41,36,297,1,11,12,267,null,1,2,257]};
