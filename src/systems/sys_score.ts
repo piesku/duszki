c394c49714d77595452e0a8adcee753f5c5f8f81
Stanisław Małolepszy <sta@malolepszy.org>
2022-09-18 21:29:29 +0200

    Simplify intro dialog

--- src/systems/sys_score.ts
+++ src/systems/sys_score.ts
@@ -38,10 +38,9 @@ export function sys_score(game: Game, delta: number) {
         let dialog = Dialog(
             game,
             `<h1>Welcome to AFTERLIFE!</h1>
-            <p>Where do Pac-Man's ghosts go when they die?</p><p>They come here — to the ghost town full of life!</p>
+            <p>Where do Pac-Man's ghosts go when they die? They come here — to the ghost town full of life!</p>
             <hr>
-            <p>Foster a community of <em>duszki</em> (Polish for ghosts) by providing them with housing, food, and work.
-               Inspect and build by left-clicking, deselect and cancel by right-clicking.</p>
+            <p>Foster a community of <em>duszki</em> (Polish for ghosts) by providing them with housing, food, and work.</p>
             `
         );
         document.body.insertAdjacentHTML("beforeend", dialog);
